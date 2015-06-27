b9ad3110c1edcc50a85df1d83283e8ba1637dba7
zephyrz4 <zephyrz2712@gmail.com>
2015-06-27 12:03:08 -0400

    initial libgdx project

---
+++ android/src/com/strayfluxinteractive/castrum/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.strayfluxinteractive.castrum.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.strayfluxinteractive.castrum.Castrum;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Castrum(), config);
+	}
+}
---
+++ desktop/src/com/strayfluxinteractive/castrum/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.strayfluxinteractive.castrum.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.strayfluxinteractive.castrum.Castrum;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Castrum(), config);
+	}
+}
